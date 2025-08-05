362dcd1b03e2ada39a749ef87ccab45001ef060d
Ian Otollo <ianotollo@gmail.com>
2025-08-05 12:21:38 +0300

    addition of 1 file

--- get-alerts.php
+++ get-alerts.php
@@ -1,14 +1,12 @@
 <?php
 header('Content-Type: application/json');
-include 'db.php'; // Use your existing DB connection script
-
-$sql = "SELECT type, message, latitude, longitude, ward FROM alerts ORDER BY date DESC";
-$result = $conn->query($sql);
-
-$alerts = [];
-while ($row = $result->fetch_assoc()) {
-    $alerts[] = $row;
-}
-echo json_encode($alerts);
-$conn->close();
+echo json_encode([
+  [
+    'type' => 'Test Alert',
+    'message' => 'This is a hardcoded alert',
+    'latitude' => 0.456,
+    'longitude' => 34.108,
+    'ward' => 'Test Ward'
+  ]
+]);
 ?>
